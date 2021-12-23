9727ecbea182c14d0cf3bf3617691e587cfc0e7f
mahmoud <mahmoud.shakouryan.dev@gmail.com>
2021-12-22 23:05:00 -0800

    changin api endpoint for deploying

--- joulahh-frontend/src/store/actions/productActions.js
+++ joulahh-frontend/src/store/actions/productActions.js
@@ -5,7 +5,7 @@ export const listProducts = () => {
   return async (dispatch) => {
     dispatch({ type: actions.PRODUCT_LIST_REQUEST });
     try {
-      const { data } = await axios.get("http://localhost:5000/api/products");
+      const { data } = await axios.get("http://joulahh.ir/api/products");
       dispatch({ type: actions.PRODUCT_LIST_SUCCESS, payload: data });
     } catch (err) {
       dispatch({ type: actions.PRODUCT_LIST_FAIL, payload: err.message });
