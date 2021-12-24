0a4edd78824f4e06e191b2c7b9e2fbfedf93e1a3
mahmoud <mahmoud.shakouryan.dev@gmail.com>
2021-12-23 23:46:53 -0800

    checking the proxy

--- joulahh-frontend/src/store/actions/productActions.js
+++ joulahh-frontend/src/store/actions/productActions.js
@@ -5,7 +5,7 @@ export const listProducts = () => {
   return async (dispatch) => {
     dispatch({ type: actions.PRODUCT_LIST_REQUEST });
     try {
-      const { data } = await axios.get("http://api.joulahh.ir/api/products");
+      const { data } = await axios.get("api/products");
       dispatch({ type: actions.PRODUCT_LIST_SUCCESS, payload: data });
     } catch (err) {
       dispatch({ type: actions.PRODUCT_LIST_FAIL, payload: err.message });
